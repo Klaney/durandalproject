f64fe3d10918a3c6dd1a112737bf4396088f64d6
Klaney <Keatonus@gmail.com>
2016-04-13 18:16:53 -0700

    user model created, tested and hashed and passed

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ var express = require("express"),
 	// db = require('./models'),
 	path = require('path'),
 	app = module.exports.app = exports.app = express(),
-	mongoose = require('mongoose');
-	// durandal = require('durandal');
+	mongoose = require('mongoose'),
+	db = mongoose.connection,
+	User = require('./models/user');
 
+mongoose.connect('mongodb://localhost/test');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/static'));
 app.use(express.static(__dirname + '/public'));
 app.use(require('connect-livereload')());
 
+db.on('error', function(){
+	console.log("We didn't connect");
+});
+db.once('open', function() {
+  console.log("We're connected!");
+});
 
-app.get('/*', function(req, res){
+app.get('/', function(req, res){
 	res.render('index.html')
-	//res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(3000);
